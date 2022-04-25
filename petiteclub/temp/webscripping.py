2f8c6828dd7c8bf350da8682f0116346f228287d
Jinho(David) Seo <brownkpt60@gmail.com>
2022-04-25 01:21:45 -0400

    updates

--- petiteclub/temp/webscripping.py
+++ petiteclub/temp/webscripping.py
@@ -22,17 +22,25 @@ from bs4 import BeautifulSoup
 # print(doc.prettify())
 #  -----------------------------------------------------------
 
-url = "https://www.anntaylor.com/petite-dresses/cata000028"
+keyword = input("Enter Keyword: ")
+keyword = keyword.strip().title()
+
+# url = "https://www.anntaylor.com/petite-dresses/cata000028"
+url ="https://www.anntaylor.com/search/searchResults.jsp?question=Petite+"+keyword+"&N=102435"
+# https://www.anntaylor.com/search/searchResults.jsp?question=Petite+Dotted&N=102435
+
+# url ="https://www.anntaylor.com/search/searchResults.jsp?goToPage=2&question=Petite+Pants&N=126314"
+# ....
 
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "lxml")
 
 # scrap title of title
-cat_title = doc.find("h1", class_="page-title")
-print(cat_title.string.strip())
+# cat_title = doc.find("h1", class_="page-title")
+# print(cat_title.string.strip())
 
 # scrap product lists
-products = doc.find_all("li", class_="product")
+# products = doc.find_all("li", class_="product")
 # print("len:", len(products))
 # print(products)
 
@@ -70,22 +78,88 @@ products = doc.find_all("li", class_="product")
 # img = products[0].find_all("img")[0]
 # print(img["src"])
 
-dresses = dict()
-for idx, product in enumerate(products):
-    url = product.select("div > a")[0].get("href")
-    name = product.select("strong")[0].string.strip()
-    price = product.select("span.price > span")[0].string
-    img = product.find_all("img")[0].get("src")
+# Get dress data from the Site and saved into dict()
+# dresses = dict()
+# for idx, product in enumerate(products):
+#     url = product.select("div > a")[0].get("href")
+#     name = product.select("strong")[0].string.strip()
+#     price = product.select("span.price > span")[0].string
+#     img = product.find_all("img")[0].get("src")
 
-    dresses[idx] = {"name": name, "url": url, "price": price, "img": img}
+#     dresses[idx] = {"name": name, "url": url, "price": price, "img": img}
+#     return print(dresses[idx])
 
-print(dresses[0])
-print(dresses[1])
-print(dresses[2])
-# print(dresses)
-print("len:", len(dresses))
+# # print(dresses[0])
+# # print(dresses[1])
+# # print(dresses[2])
+# # print(dresses)
+# print("len:", len(dresses))
 
+names = []
+prices = []
+urls = []
+imges = []
 
+def get_dress_ann(url):
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "lxml")
+
+    products = doc.find_all("li", class_="product")
+    
+    for idx, product in enumerate(products):
+        name = product.select("strong")[0].string.strip()
+        # name = product.select("strong")[0].string.strip()
+        # print(name)
+        if keyword in name:
+            url = product.select("div > a")[0].get("href")
+            price = product.select("span.price > span")[0].string
+            img = product.find_all("img")[0].get("src")
+
+        names.append(name)
+        prices.append(price)
+        urls.append(url)
+        imges.append(img)
+        
+        # print(name)
+        # print(price)
+        # print(url)
+        # print(img)
+    return names, prices, urls, imges
+
+def get_dress_ann1(url):
+
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, "lxml")
+    products = doc.find_all("li", class_="product")
+    
+    print("products len:", len(products))
+
+    dresses = dict()
+    for idx, product in enumerate(products):
+        # name = product.select("strong")[0].string.strip()
+        name = product.select("strong")[0].string.strip()
+        
+        if keyword in name:
+            url = product.select("div > a")[0].get("href")
+            price = product.select("span.price > span")[0].string
+            img = product.find_all("img")[0].get("src")
+
+            dresses[idx] = {"name": name, "url": url, "price": price, "img": img}
+            print(dresses[idx])
+    
+    # print(dresses[0])
+    # print(dresses[1])
+    # print(dresses[2])
+    # print(dresses)
+    # print("len:", len(dresses))
+    # return dresses
+
+# data = get_dress_ann(url)
+# print(data)
+
+get_dress_ann(url)
+print("----------------")
+get_dress_ann1(url)
 
 #  -----------------------------------------------------------
 # driver.quit()
