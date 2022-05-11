1422c884208729f75452ede2bef6f8f0fe09bdb7
Jinho(David) Seo <brownkpt60@gmail.com>
2022-05-11 14:02:21 -0400

    black

--- petiteclub/favorites/views.py
+++ petiteclub/favorites/views.py
@@ -1,38 +1,30 @@
 from django.shortcuts import render
 
-# Create your views here.
-
-# def home(request):
-#     return render(request, "search/home.html", context)
+# load from search model(db)
 
 products = [
     {
-            "product_name": "Some Dress1",
-            "product_price": "$100.00",
-            "product_url": "anntalyor.com/some_dress.html",
-            "product_img": "anntalyer.com/img/some_dress.jpg"
+        "product_name": "Some Dress1",
+        "product_price": "$100.00",
+        "product_url": "anntalyor.com/some_dress.html",
+        "product_img": "anntalyer.com/img/some_dress.jpg",
     },
     {
-            "product_name": "Some Dress2",
-            "product_price": "$200.00",
-            "product_url": "anntalyor.com/some_dress.html",
-            "product_img": "anntalyer.com/img/some_dress.jpg"
+        "product_name": "Some Dress2",
+        "product_price": "$200.00",
+        "product_url": "anntalyor.com/some_dress.html",
+        "product_img": "anntalyer.com/img/some_dress.jpg",
     },
     {
-            "product_name": "Some Dress3",
-            "product_price": "$300.00",
-            "product_url": "anntalyor.com/some_dress.html",
-            "product_img": "anntalyer.com/img/some_dress.jpg"
+        "product_name": "Some Dress3",
+        "product_price": "$300.00",
+        "product_url": "anntalyor.com/some_dress.html",
+        "product_img": "anntalyer.com/img/some_dress.jpg",
     },
-
 ]
 
 
-
-
-context = {
-    "products": products
-}
+context = {"products": products}
 
 
 def favorites(request):
